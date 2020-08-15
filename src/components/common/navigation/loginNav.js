e052e1a8e5e017f87ee0bb39fa21eec552f49b1a
jideabdqudus <jideabdqudus@gmail.com>
2020-08-15 02:10:55 +0100

    Added Commit

---
+++ src/components/common/navigation/loginNav.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+
+import {
+  Navbar,
+  NavbarBrand,
+  Container,
+} from "reactstrap"
+
+export default class LoginNavigation extends Component {
+  render() {
+    return (
+      <Container>
+        <Navbar color="light" light expand="md" s sticky>
+          <NavbarBrand href="/">Moneyvest</NavbarBrand>
+        </Navbar>
+      </Container>
+    )
+  }
+}
--- src/pages/sign.js
+++ src/pages/sign.js
@@ -1,47 +1,102 @@
-import React, { Fragment } from 'react'
-import styles from './sign.module.css'
-import {Container, FormGroup} from 'reactstrap'
-import cx from 'classnames'
+import React, { Fragment, useState } from "react"
+import styles from "./sign.module.css"
+import { Container, Row, Col, FormGroup } from "reactstrap"
+import cx from "classnames"
+import LoginNavigation from "../components/common/navigation/loginNav"
 
 const sign = () => {
-    return (
-        <Fragment>
-        <div className={styles.pageWrapper}>
-        <div className={styles.pageContentBge5}>
-        <Container>
-            <div className={styles.container}>
-                <div className={styles.loginWrap}>
-                    <div className={styles.loginContent}>
-                        
-                        <div  className={styles.loginForm}>
-                            <form>
-                            <FormGroup>
-                                    <label for="user">User</label>
-                                    <input className={cx(styles.auInput, styles.auInputFull)} id="password" type="email" name="email" placeholder="Email"/>
-                                </FormGroup>
-                                <FormGroup>
-                                <label for="password">Password</label>
-                                    <input className={cx(styles.auInput, styles.auInputFull)} id="password" type="password" name="password" placeholder="Password"/>
-                                </FormGroup>
-                                    <button style={{marginBottom:"20px"}} className={cx(styles.auBtn, styles.auBtnBlock, styles.auBtnGreen)} type="submit">sign in</button>
-                                
-                            </form>
-                            <div className={styles.registerLink}>
-                                <p>
-                                    User & Password: 
-                                    <a href="!#">{" "}guest</a>
-                                </p>
-                            </div>
+    const [user, setUser] = useState(initialState)
+  return (
+    <Fragment>
+      <LoginNavigation />
+      <div
+        style={{ backgroundColor: "#098c8c" }}
+      >
+        <div
+          className={styles.pageContentBge5}
+        >
+          <Container>
+            <div className={styles.container} >
+              <div className={styles.loginWrap} >
+                <div className={styles.loginContent} style={{ borderRadius: "10px" }}>
+                  <Row>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                      <h4>Sign into Moneyvest</h4>
+                    </Col>
+                  </Row>
+                  <br />
+                  <div className={styles.loginForm}>
+                    <form>
+                      <FormGroup>
+                        <label for="user">User</label>
+                        <input
+                          className={cx(styles.auInput, styles.auInputFull)}
+                          id="password"
+                          type="email"
+                          name="email"
+                          placeholder="Email"
+                        />
+                      </FormGroup>
+                      <FormGroup>
+                        <label for="password">Password</label>
+                        <input
+                          className={cx(styles.auInput, styles.auInputFull)}
+                          id="password"
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                        />
+                      </FormGroup>
+                      <button
+                        style={{ marginBottom: "20px" }}
+                        className={cx(
+                          styles.auBtn,
+                          styles.auBtnBlock,
+                          styles.auBtnGreen
+                        )}
+                        type="submit"
+                      >
+                        sign in
+                      </button>
+                      <div className={styles.socialLoginContent}>
+                        <div className={styles.socialButton}>
+                          <button
+                            className={cx(
+                              styles.auBtn,
+                              styles.auBtnBlock,
+                              styles.auBtnBlue
+                            )}
+                          >
+                            sign in with facebook
+                          </button>
+                          <br />
+                          <button
+                            className={cx(
+                              styles.auBtn,
+                              styles.auBtnBlock,
+                              styles.auBtnBlue2
+                            )}
+                          >
+                            sign in with twitter
+                          </button>
                         </div>
+                      </div>
+                    </form>
+                    <div className={styles.registerLink}>
+                      <p>
+                        User & Password:
+                        <a href="!#"> {"  "}guest</a>
+                      </p>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            </Container>
+          </Container>
         </div>
-
-    </div>  
-        </Fragment>
-    )
+      </div>
+    </Fragment>
+  )
 }
 
 export default sign
